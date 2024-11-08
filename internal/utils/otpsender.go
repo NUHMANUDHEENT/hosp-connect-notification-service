4a80870deb155d5b55f3dcf7c44d9926cf46d0fe
nuhmanudheen t <nuhmanudheent@gmail.com>
2024-11-08 13:58:07 +0530

    updates

---
+++ internal/utils/otpsender.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+
+	"gopkg.in/gomail.v2"
+)
+
+// Send email function using GoMail
+func SendNotificationToEmail(email, subject, body string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", "nuhmotp@gmail.com")
+	m.SetHeader("To", email)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+
+	d := gomail.NewDialer("smtp.gmail.com", 587, os.Getenv("APPEMAIL"), os.Getenv("APPPASSWORD"))
+
+	if err := d.DialAndSend(m); err != nil {
+		return err
+	}
+	return nil
+}
