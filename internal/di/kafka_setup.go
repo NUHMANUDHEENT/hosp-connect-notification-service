10df36eace8b823ee640fa82f5700c46c65c5b31
nuhmanudheen t <nuhmanudheent@gmail.com>
2024-11-21 20:23:49 +0530

    update html path

--- internal/di/kafka_setup.go
+++ internal/di/kafka_setup.go
@@ -12,16 +12,17 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
-func NewKafkaConsumer(broker,  topic string) (*kafka.Reader, error) {
+func NewKafkaConsumer(broker, topic string, partition int) (*kafka.Reader, error) {
 
 	reader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:     []string{broker},
+		Brokers: []string{broker},
 		// GroupID:     groupID,
 		Topic:       topic,
-		StartOffset: kafka.FirstOffset, 
-		MinBytes:    10e3,           
-		MaxBytes:    10e6,            
-		MaxWait:     1 * time.Second,  
+		Partition:   partition,
+		StartOffset: kafka.FirstOffset,
+		MinBytes:    10e3,
+		MaxBytes:    10e6,
+		MaxWait:     1 * time.Second,
 	})
 
 	return reader, nil
@@ -32,21 +33,20 @@ func KafkaSetup() {
 	db := config.InitDatabase()
 	repo := repository.NewNotificationRepo(db)
 
-	paymentConsumer, err := NewKafkaConsumer(os.Getenv("KAFKA_BROKER"), "payment_topic")
+	paymentConsumer, err := NewKafkaConsumer(os.Getenv("KAFKA_BROKER"), "payment_topic", 0)
 	if err != nil {
 		log.Fatalf("Failed to create Kafka consumer for payment topic: %v", err)
 	}
 
-	appointmentConsumer, err := NewKafkaConsumer(os.Getenv("KAFKA_BROKER"), "appointment_topic")
+	appointmentConsumer, err := NewKafkaConsumer(os.Getenv("KAFKA_BROKER"), "appointment_topic", 0)
 	if err != nil {
 		log.Fatalf("Failed to create Kafka consumer for appointment topic: %v", err)
 	}
 
-	appointmentAlertConsumer, err := NewKafkaConsumer(os.Getenv("KAFKA_BROKER"),"alert_topic")
+	appointmentAlertConsumer, err := NewKafkaConsumer(os.Getenv("KAFKA_BROKER"), "alert_topic", 1)
 	if err != nil {
 		log.Fatalf("Failed to create Kafka consumer for alert topic: %v", err)
 	}
-
 
 	srv := service.NewNotificationService(repo, paymentConsumer, appointmentConsumer, appointmentAlertConsumer)
 	handl := handler.NewNotificationHandler(srv)
@@ -57,7 +57,7 @@ func KafkaSetup() {
 			log.Fatalf("Error in payment consumer: %v", err)
 		}
 	}()
-	
+
 	// go func() {
 	// 	err := handl.AppointmentHandler()
 	// 	if err != nil {
